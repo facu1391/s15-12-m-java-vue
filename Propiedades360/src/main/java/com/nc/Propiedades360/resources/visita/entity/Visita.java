b69a67fd3fab92aab5ff7e44d61c474544d3141d
Emiliano <86033466+EmiBarrientos@users.noreply.github.com>
2024-06-06 14:24:05 -0300

    Update Visita.java
    agregando codigo de visita traido desde trabajoBackend

--- Propiedades360/src/main/java/com/nc/Propiedades360/resources/visita/entity/Visita.java
+++ Propiedades360/src/main/java/com/nc/Propiedades360/resources/visita/entity/Visita.java
@@ -2,44 +2,40 @@ package com.nc.Propiedades360.resources.visita.entity;
 
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.nc.Propiedades360.resources.Cliente.Entity.Cliente;
+import com.nc.Propiedades360.resources.inmueble.entity.Inmueble;
 import com.nc.Propiedades360.resources.reserva.entity.Reserva;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
-public class Visita extends Reserva {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visita {
 
-    private String propietario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date fechaVisita;
 
-    // Constructor
-    public Visita() {
-    }
+    @JsonBackReference(value = "cliente-visitas")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
 
-    public Visita(LocalDate fechaInicio, LocalDate fechaFin, Long clienteId, Long inmuebleId, String propietario) {
-        super.setFechaInicio(fechaInicio);
-        super.setFechaFin(fechaFin);
-        super.setClienteId(clienteId);
-        super.setInmuebleId(inmuebleId);
-        this.propietario = propietario;
-        super.setEstado(Estado.PENDIENTE);
-    }
+    @JsonBackReference(value = "inmueble-visitas")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inmueble_id")
+    private Inmueble inmueble;
 
-    // Getters y Setters
-    public String getPropietario() {
-        return propietario;
-    }
 
-    public void setPropietario(String propietario) {
-        this.propietario = propietario;
-    }
-
-    // Métodos adicionales para Visita
-    public void confirmarVisita() {
-        this.setEstado(Estado.CONFIRMADA);
-    }
-
-    public void cancelarVisita() {
-        this.setEstado(Estado.CANCELADA);
-    }
 }
