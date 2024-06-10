138d885a4972d08f0068eb49658786b2b926f0d1
Ale0602 <126637451+Ale0602@users.noreply.github.com>
2024-06-10 10:10:50 -0300

    Añadiendo páginas

---
+++ Propiedad360/src/pages/Inicio/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InicioSesión from '../../components/InicioSesión/InicioSesión';
+
+const Inicio = () => {
+  return (
+    <section className="bg-white">
+      <InicioSesión/>
+    </section>
+  )
+}
+
+export default Inicio;
---
+++ Propiedad360/src/pages/Registro/Registro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistroCuenta from '../../components/RegistroCuenta/RegistroCuenta';
+
+const Registro = () => {
+  return (
+    <section className="bg-white">
+      <RegistroCuenta/>
+    </section>
+  )
+}
+    
+export default Registro;
